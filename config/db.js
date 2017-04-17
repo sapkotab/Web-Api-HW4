3950ed7fc436ceff7daf180ff33e0e4233b0f536
sapkotab <bhuwan.sapkota@ucdenver.edu>
2017-04-17 08:37:18 -0600

    revert to right one

--- config/db.js
+++ config/db.js
@@ -4,40 +4,60 @@
 'use strict;'
 var mongoose = require('mongoose');
 
-var reviewSchema = new mongoose.Schema({
-    movie:      {type: String, required: true},
-    reviewer:   {type: String, required: true},
-    rating:     {type: Number, required:true, min: 0, max: 5},
-    text:       {type: String, required: true},
-})
-
-
-var movieSchema = new mongoose.Schema({
-    Title:          {type: String, required: true, unique: true},
-    YearReleased:   {type: Number, required: true},
-    Actors:         [{
-                        Name: {type: String, required: true}
-                    }]
-})
+// to create encrypted password
+// var vault = require('avault').createVault(__dirname);
+// var keyName = 'key1';
+// vault.generateKey(keyName).then(
+//     function (keyResponse) {
+//         vault.store(keyName, '{"username": "<put db username here>", "password": "<put db user password here>"}', 'GitVault').then(
+//             function (storeResponse) {
+//                 console.log('Ok', storeResponse);
+//             },
+//             function (err) {
+//                 console.log('Error', err);
+//             });
+//     },
+//     function (err) {
+//         console.log('Error', err);
+//     });
 
 var vault = require('avault').createVault(__dirname);
 vault.get('GitVault', function (profileString) {
-    if (!profileString) {
+    if(!profileString){
         console.log('Error: required vault is not found');
-    } else {
+    }else {
         var profile = JSON.parse(profileString);
         console.log(profile);
     }
     var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
-        replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
+    replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
 
-    var moviedb = mongoose.createConnection('mongodb://' + profile.username +':' + profile.password + '@ds149700.mlab.com:49700/movie',options);
-    var Movie = moviedb.model('movies',movieSchema );
-    var Review = moviedb.model('reviews', reviewSchema);
+    var dbHost = 'mongodb://' + profile.username +':' + profile.password + '@ds149700.mlab.com:49700/movie';
 
-    module.exports = {
-        Movies: Movie,
-        Reviews: Review
-    };
+        mongoose.connect(dbHost, options);
+    });
+
+//
+// var dbHost = 'mongodb://localhost/hw4movie'
+//
+// mongoose.connect(dbHost);
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error'))
+
+db.once('open', function () {
+    //console.log("Connected to DB");
+});
+var ActorSchema = new mongoose.Schema({
+    Name: {type: String, required: true}
 })
 
+var movieSchema = new mongoose.Schema({
+    Title: {type: String, required: true, unique: true},
+    YearReleased: {type: Number, required: true},
+    Actors: [ActorSchema]
+})
+
+
+module.exports = mongoose.model('Movie', movieSchema)
