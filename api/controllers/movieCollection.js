a1e666632bf771a1142167bc0cba9c49353d03a7
sapkotab <bhuwan.sapkota@ucdenver.edu>
2017-04-17 08:14:43 -0600

    analytics left

--- api/controllers/movieCollection.js
+++ api/controllers/movieCollection.js
@@ -4,10 +4,13 @@
 
 'use strict;'
 
-var Movies = require('../../config/db')
+var db = require('../../config/db');
+var async = require('async')
 
 
 module.exports = {
+    saveReview:saveReview,
+    getrev:getrev,
     getAll : getAll,
     saveit : saveit,
     getOne : getOne,
@@ -15,55 +18,121 @@ module.exports = {
     delMovie : delMovie
 };
 
-function getAll(req,res,next){
-    Movies.find({},function(err,response){
-        if(err) throw {err:err};
-        var allMovie = response;
-        res.send({Movielist: allMovie});
-    });
+//POST /movie operationId
+function saveReview(req, res, next) {
+    var movie = req.body.movie;
+    // make sure movie doesn't exit in database before saving
+    db.Movies.find({Title: movie}, function (err, response) {
+        if (err) throw {err:err};
+        if (response.length === 0) {
+            res.send({Error: "Movie does not exist for review"})
+        }
+        else {
+            var newreview = new db.Reviews(req.body);
+            newreview.save(function (err) {
+                if (err) throw {err: err}
+            })
+            res.send({success: "New review successfully added to database"})
+        }
+    })
 }
+
+function getrev(req, res, next) {
+    db.Reviews.find({}, function (err, reviews) {
+        if (err) throw {err: err};
+        res.send({Reviewlist: reviews})
+    })
+}
+
+function getAll(req,res,next) {
+    if (req.query.review === 'true') {
+        db.Movies.aggregate([
+            {
+                $lookup: {
+                    from: "reviews",
+                    localField: "Title",
+                    foreignField: "movie",
+                    as: "review"
+                }
+            }
+        ], function (err, result) {
+            if (err) throw {err: err};
+            else {
+                res.send({Movielist: result});
+            }
+        });
+    }
+    else {
+        db.Movies.find({}, function (err, movies) {
+            if (err) throw {err: err};
+            res.send({Movielist: movies})
+        })
+    }
+}
+
 
 //POST /movie operationId
 function saveit(req, res, next) {
     var mTitle = req.body.Title;
-    // make sure movie doesn't exit in database before saving
-    Movies.find({Title: mTitle}, function (err, response) {
-        if (err) throw {err:err};
-        if (response.length === 0) {
+    if(req.body.Actors.length < 3){
+        res.send({Error: "At least 3 actors required"})
+    }
+    else {
+        // make sure movie doesn't exit in database before saving
+        db.Movies.find({Title: mTitle}, function (err, response) {
+            if (err) throw {err: err};
+            if (response.length === 0) {
 
-            var newMovie = new Movies(req.body);
-            newMovie.save(function (err) {
-                if (err) throw {err:err};
-            });
-            res.send({success: "New movie successfully added to database"})
-        }
-        else
-            res.send({message: "Movie already exist"})
-    })
+                var newMovie = new db.Movies(req.body);
+                newMovie.save(function (err) {
+                    if (err) throw {err: err};
+                });
+                res.send({success: "New movie successfully added to database"})
+            }
+            else
+                res.send({message: "Movie already exist"})
+        })
+    }
 }
 //GET /movie/{id} operationId
 function getOne(req, res, next) {
     var Title = req.swagger.params.Title.value; //req.swagger contains the path parameters
-    var movie = Movies.find({Title : Title}, function (err,response) {
-        if (err) throw {err:err};
-        if (response.length === 0){
-            res.send({message: "No such movie is found"})
-        }
-        else
-        res.send({movie: response})
-    });
+    if (req.query.review === 'true') {
+        db.Movies.aggregate([
+            { $match : { Title : Title } },
+            {
+                $lookup: {
+                    from: "reviews",
+                    localField: "Title",
+                    foreignField: "movie",
+                    as: "review"
+                }
+            }
+        ], function (err, result) {
+            if (err) throw {err: err};
+            else {
+                res.send({Movie: result});
+            }
+        });
+    }
+    else {
+        db.Movies.find({ Title : Title }, function (err, movie) {
+            if (err) throw {err: err};
+            res.send({Movie: movie})
+        })
+    }
 }
 //PUT /movie/{id} operationId
 function update(req, res, next) {
     var mTitle = req.swagger.params.Title.value; //req.swagger contains the path parameters
     // make sure movie doesn't exit in database before saving
-    Movies.find({Title: mTitle}, function (err, movie) {
+    db.Movies.find({Title: mTitle}, function (err, movie) {
         if (err) throw {err:err};
         if (movie.length === 0) {
             res.send({message: "No such movie is found"})
         }
         else {
-            Movies.findOneAndUpdate({Title: mTitle},{$set: req.body},{new: true}, function (err, numUpdated) {
+            db.Movies.findOneAndUpdate({Title: mTitle},{$set: req.body},{new: true}, function (err, numUpdated) {
                // console.log(updated)
             });
              res.send({message: "Movie is updated"})
@@ -74,14 +143,14 @@ function update(req, res, next) {
 //DELETE /movie/{id} operationId
 function delMovie(req, res, next) {
     var Title = req.swagger.params.Title.value; //req.swagger contains the path parameters
-    Movies.find({Title: Title}, function (err, response) {
+    db.Movies.find({Title: Title}, function (err, response) {
         if (err) throw {err:err};
         // if movie doesn't exist send the message
         if (response.length === 0) {
             res.send({message: "No such movie is found"})
         }
         else {
-            Movies.remove({ Title: Title }, function(err) {
+            db.Movies.remove({ Title: Title }, function(err) {
                if(err) throw {err:err};
                res.send({message: "Movie successfully deleted"})
             });
--- config/store.js
+++ config/store.js
@@ -1,6 +1,6 @@
 module.exports = {
 	"GitVault": {
 		"key": "key1",
-		"data": "95ca3d6beec68441a4f9c0b9ddea7f184a82113e77a853fa211d2ee7b1b91f8cff6f41520002c0fa73ea9a0820dd9385"
+		"data": "36a786734eeb5f179c8adcbfcdec2edd3939cbd0b58f434942cb83081925d29721ec755d59be2f65f072476d5f4dd319"
 	}
 };
